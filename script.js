2775f3f5d2979d1aba1c9ed72eb37252e025e149
gauvainboiche <gauvain.boiche+git@protonmail.com>
2024-10-10 10:36:51 +0200

    First init

---
+++ script.js
@@ -0,0 +1,15 @@
+let BACKEND_URL = 'https://nnp3gjpmpu.us-east-1.awsapprunner.com';
+
+
+if (BACKEND_URL[BACKEND_URL.length - 1] === '/') {
+  BACKEND_URL = BACKEND_URL.slice(0, -1);
+}  
+
+setInterval(function () {
+  fetch(`${BACKEND_URL}/time`)
+    .then(response => response.json())
+    .then(data => {
+      const timer = document.getElementById('timer');
+      timer.innerHTML = data.time;
+    });
+}, 1000);
